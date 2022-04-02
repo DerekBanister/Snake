58b70ea039d750c4df1f636f81ca22a510537264
unknown <ptownderek@yahoo.com>
2022-04-02 09:50:31 -0700

    setting directional movement for snake using x/y and unitsize

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -6,9 +6,11 @@ import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements ActionListener{
 	//vars
-	static final int SCREEN_WIDTH = 650;
-	static final int SCREEN_HEIGHT = 650;
-	static final int UNIT_SIZE = 30;
+	//screen dimensions
+	static final int SCREEN_WIDTH = 600;
+	static final int SCREEN_HEIGHT = 600;
+	//Determines size of program
+	static final int UNIT_SIZE = 25;
 	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
 	static final int DELAY = 75;
 	final int x[] = new int[GAME_UNITS];
@@ -20,7 +22,7 @@ public class GamePanel extends JPanel implements ActionListener{
 	// y coord
 	int appleY;
 	//r for right
-	char direction = 'R';
+	char direction = 'D';
 	boolean running = false;
 	Timer timer;
 	Random random;
@@ -45,13 +47,40 @@ public class GamePanel extends JPanel implements ActionListener{
 		draw(g);
 	}
 	public void draw(Graphics g) {
-		
+		//turn board into grid
+		for(int i =0; i<SCREEN_HEIGHT/UNIT_SIZE;i++) {
+			g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+			g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+			//	
+		}
+		g.setColor(Color.green);
+		g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 	}
-	public void newApple() {
 		
+	public void newApple() {
+		appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+		appleX = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
 	}
 	public void moveSnake() {
-		
+		for (int i = bodyParts; i>0; i--) {
+			//shifting coords in array by 1
+			x[i] = x[i-1];
+			y[i] = y[i-1];
+		}
+		switch(direction) {
+		case 'W':
+			y[0] = y[0] - UNIT_SIZE;
+			break;
+		case 'S':
+			y[0] = y[0] + UNIT_SIZE;
+			break;
+		case 'A':
+			x[0] = x[0] - UNIT_SIZE;
+			break;
+		case 'D':
+			x[0] = x[0] - UNIT_SIZE;
+			break;
+		}
 	}
 	public void checkApple() {
 		
