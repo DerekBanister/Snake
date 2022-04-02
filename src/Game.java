58fbd439a6e9738a44ef0ccbc1be3141f5ca0be6
unknown <ptownderek@yahoo.com>
2022-04-01 22:58:24 -0700

    beginning to build gamepanel, declaring global vars

--- src/Game.java
+++ src/Game.java
@@ -5,5 +5,12 @@ public class Game extends JFrame {
 //		GamePanel panel = new GamePanel();
 //		this.add(panel);
 		this.add(new GamePanel());
+		this.setTitle("Snake");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		//make panel appear in middle of computer
+		this.setLocationRelativeTo(null);
 	}
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,14 +1,74 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements ActionListener{
-
+	//vars
+	static final int SCREEN_WIDTH = 650;
+	static final int SCREEN_HEIGHT = 650;
+	static final int UNIT_SIZE = 30;
+	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
+	static final int DELAY = 75;
+	final int x[] = new int[GAME_UNITS];
+	final int y[] = new int[GAME_UNITS];
+	int bodyParts = 6;
+	int applesAte;
+	//x coord of apple
+	int appleX;
+	// y coord
+	int appleY;
+	//r for right
+	char direction = 'R';
+	boolean running = false;
+	Timer timer;
+	Random random;
+	
+	
+	
+	//constructor
+	GamePanel(){
+		random = new Random();
+		this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+		this.setBackground(Color.black);
+		this.setFocusable(true);
+		this.addKeyListener(new MyKeyAdapter());
+		startGame();
+	}
+	public void startGame() {
+		
+	}
+	public void paintComponent(Graphics g) {
+		
+	}
+	public void draw(Graphics g) {
+		
+	}
+	public void newApple() {
+		
+	}
+	public void moveSnake() {
+		
+	}
+	public void checkApple() {
+		
+	}
+	public void checkCollision() {
+		
+	}
+	public void gameOver(Graphics g) {
+		
+	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
+	}
+	public class MyKeyAdapter extends KeyAdapter{
+		@Override
+		public void keyPressed(KeyEvent e) {
+		}
 	}
 
 }
